28f504c19efbce0feae6d2e21abd5746be470e4d
LukaszGruba <lukgru@gmail.com>
2016-11-28 08:44:32 +0100

    GAlgo init + test

---
+++ src/main/java/com/lukgru/galgo/GAlgo.java
@@ -0,0 +1,7 @@
+package com.lukgru.galgo;
+
+/**
+ * Created by Lukasz on 28.11.2016.
+ */
+public class GAlgo {
+}
---
+++ src/test/java/com/lukgru/galgo/GAlgoTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lukasz on 28.11.2016.
+ */
+public class GAlgoTest {
+
+}
