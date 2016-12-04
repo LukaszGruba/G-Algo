35ebf35fba703e2559b891f294b25c3314461280
LukaszGruba <lukgru@gmail.com>
2016-12-04 17:32:21 +0100

    basic acceptance test - solving simple equation

--- src/test/java/com/lukgru/galgo/GAlgoTest.java
+++ src/test/java/com/lukgru/galgo/GAlgoTest.java
@@ -1,31 +1,10 @@
 package com.lukgru.galgo;
 
-import com.lukgru.galgo.runner.GenerationResult;
-import org.junit.Test;
-
-import java.util.Arrays;
-import java.util.Collection;
+import static org.junit.Assert.*;
 
 /**
- * Created by Lukasz on 28.11.2016.
+ * Created by Lukasz on 04.12.2016.
  */
 public class GAlgoTest {
 
-    @Test
-    public void testAPI() {
-        //given
-        Collection<Integer> initPopulation = Arrays.asList(1, 10, 100, 1000, 10000, 100000);
-
-        //when
-        GenerationResult result = GAlgo.fromPopulation(initPopulation)
-                .withFitnessFunction((a) -> 123 - a).targeting(0)
-                .withCrossover((a, b) -> (a + b) / 2)
-                .withMutationFunction((a) -> (a)).withMutationProbability(0.05d)
-                .runner().generate();
-
-        //then
-        result.getBest();
-        result.getIterations();
-        result.getFinalPopulation();
-    }
 }
