b756d79e0d0d528673a5247017ce44be3f9412cf
LukaszGruba <lukgru@gmail.com>
2016-12-26 14:25:19 +0100

    SimpleSelectionRunner tests + implementation

--- src/main/java/com/lukgru/galgo/runner/selection/SimpleSelectionRunner.java
+++ src/main/java/com/lukgru/galgo/runner/selection/SimpleSelectionRunner.java
@@ -1,14 +1,20 @@
 package com.lukgru.galgo.runner.selection;
 
+import static java.util.stream.Collectors.toList;
+
 import com.lukgru.galgo.fitness.FitnessFunction;
+import com.lukgru.galgo.population.Individual;
 import com.lukgru.galgo.population.Population;
+
+import java.util.List;
+
 
 /**
  * Created by Lukasz on 12.12.2016.
  */
-@SuppressWarnings("PMD")
 public class SimpleSelectionRunner<T> implements SelectionRunner<T> {
 
+    //TODO: replace with target
     private FitnessFunction<T> fitnessFunction;
 
     public SimpleSelectionRunner(FitnessFunction<T> fitnessFunction) {
@@ -17,8 +23,17 @@ public class SimpleSelectionRunner<T> implements SelectionRunner<T> {
 
     @Override
     public Population<T> selectForReproduction(Population<T> population) {
-        //TODO: implement
-        return null;
+        Double target = fitnessFunction.getTarget();
+        List<T> selected = population.getIndividuals().stream()
+                .sorted((indA, indB) -> {
+                    Double fitnessA = Math.abs(target - indA.getFitnessScore());
+                    Double fitnessB = Math.abs(target - indB.getFitnessScore());
+                    return fitnessA.compareTo(fitnessB);
+                })
+                .map(Individual::getValue)
+                .limit((int)Math.ceil(population.size() / 2.0))
+                .collect(toList());
+        return new Population<>(selected);
     }
     
 }
