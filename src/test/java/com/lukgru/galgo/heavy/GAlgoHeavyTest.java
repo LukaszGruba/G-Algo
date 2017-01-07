58692438478e2b28634afa88274b1968230d16b6
LukaszGruba <lukgru@gmail.com>
2017-01-07 15:14:15 +0100

    remove TODO as already completed

--- src/test/java/com/lukgru/galgo/heavy/GAlgoHeavyTest.java
+++ src/test/java/com/lukgru/galgo/heavy/GAlgoHeavyTest.java
@@ -3,7 +3,6 @@ package com.lukgru.galgo.heavy;
 /**
  * Created by Lukasz on 04.12.2016.
  */
-//TODO: create profile to run comparison with random
 //TODO: create comparisons with brute force or random
 //TODO: add more challenging tests (e.g. with non zero target, single solution with many variables etc.)
 //TODO: remove this class
