819557d1bb4bc334f9c3b0248908f5e3b2425304
LukaszGruba <lukgru@gmail.com>
2017-01-10 15:57:08 +0100

    HeavyTestUtils added method for comparing execution times

--- src/test/java/com/lukgru/galgo/heavy/HeavyTestUtils.java
+++ src/test/java/com/lukgru/galgo/heavy/HeavyTestUtils.java
@@ -13,4 +13,17 @@ public class HeavyTestUtils {
         return Math.abs(fitness - target) < epsilon;
     }
 
+    public static long compareExecution(Runnable test1, Runnable test2) {
+        long startTime = System.currentTimeMillis();
+        test1.run();
+        long switchTime = System.currentTimeMillis();
+        test2.run();
+        long stopTime = System.currentTimeMillis();
+
+        long test1Time = switchTime - startTime;
+        long test2Time = stopTime - switchTime;
+        System.out.println("GA = " + test1Time + "ms, random = " + test2Time + "ms");
+        return test1Time - test2Time;
+    }
+
 }
