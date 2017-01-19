2e8cc90c735a175d6fa27841dc282dcfaaeff323
LukaszGruba <lukgru@gmail.com>
2017-01-19 08:11:25 +0100

    util methods for generating random Strings

--- src/test/java/com/lukgru/galgo/heavy/HeavyTestUtils.java
+++ src/test/java/com/lukgru/galgo/heavy/HeavyTestUtils.java
@@ -2,6 +2,7 @@ package com.lukgru.galgo.heavy;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import static java.util.stream.Collectors.toList;
 
@@ -55,4 +56,43 @@ public class HeavyTestUtils {
         long end = System.currentTimeMillis();
         return end - start;
     }
+
+    public static String randomStringUpTo20Chars() {
+        return randomStringNChars((int)(20.0 * Math.random()));
+    }
+
+    public static String randomStringUpTo40Chars() {
+        return randomStringNChars((int)(40.0 * Math.random()));
+    }
+
+    public static String randomStringUpToNChars(int n) {
+        return randomStringNChars((int) (n * Math.random()));
+    }
+
+    public static String randomStringNChars(int length) {
+        Random rnd = new Random();
+        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,()[]-";
+        char[] chars = new char[length];
+        for (int i=0 ; i<length ; i++) {
+            chars[i] = alphabet.charAt(rnd.nextInt(alphabet.length()));
+        }
+        return new String(chars);
+    }
+
+    public static double wordsDistance(String word1, String word2) {
+        int totalDiff = 0;
+        int length1 = word1.length();
+        int length2 = word2.length();
+        for (int i = 0; i< length1 && i < length2; i++) {
+            char c1 = word1.charAt(i);
+            char c2 = word2.charAt(i);
+            int diff = Math.abs(c1 - c2);
+            if (diff > 0) {
+                totalDiff += diff + 500;
+            }
+        }
+        totalDiff += Math.abs(length2 - length1) * 1000;
+        return totalDiff;
+    }
+
 }
